018b281e9357170918c7f368b6cfb4ef4fea6006
michellele994 <michellele@email.arizona.edu>
2018-03-14 16:12:19 -0700

    added to controllers

---
+++ controllers/user.js
@@ -0,0 +1,34 @@
+var db = require("./../models");
+var allUsers = function(res) {
+    db.User.find()
+    .then(function(dbUsers) {
+        res.json(dbUsers);
+    })
+    .catch(function(err){
+        return res.json(err);
+    });
+}
+var oneUser = function(username, res) {
+    db.User.findOne({username: username})
+    .then(function(dbUsers) {
+        res.json(dbUsers);
+    })
+    .catch(function(err){
+        res.json(err);
+    });
+}
+var createUser = function(req, res) {
+    db.User.create(req.body)
+    .then(function(dbUser) {
+        res.json(dbUser);
+    })
+    .catch(function(err){
+        res.json(err);
+    });
+}
+
+module.exports = {
+    allUsers: allUsers,
+    oneUser: oneUser,
+    createUser: createUser
+}
--- routes/api/api-users.js
+++ routes/api/api-users.js
@@ -1,32 +1,13 @@
-var db = require("./../../models");
 var controller = require("./../../controllers/user.js")
 module.exports = function(router)
 {
 	router.get("/api/users/", function(req, res) {
-			db.User.find()
-			.then(function(dbUsers) {
-				res.json(dbUsers);
-			})
-			.catch(function(err){
-				res.json(err);
-			});
+		controller.allUsers(res);
 	});
 	router.get("/api/users/:username", function(req, res) {
-			db.User.findOne({username: req.params.username})
-			.then(function(dbUsers) {
-				res.json(dbUsers);
-			})
-			.catch(function(err){
-				res.json(err);
-			});
+		controller.oneUser(req.params.username, res);
 	});
 	router.post("/api/users/", function(req, res) {
-		db.User.create(req.body)
-		.then(function(dbUser) {
-			res.json(dbUser);
-		})
-		.catch(function(err){
-			res.json(err);
-		});
+		controller.createUser(req, res);
 	});
 }
