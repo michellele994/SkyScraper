fab0108ea72b06c3139cbc2d29bac6ed1e53d523
michellele994 <michellele@email.arizona.edu>
2018-03-16 01:26:36 -0700

    Added saved button, will be working on save db

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -3,6 +3,33 @@ $(function() {
     $("#scrape-button").on("click", function() {
         $.get("/scrape").then(function(response){
             console.log("HALLO");
+            $.get("/api/articles/").then(function(resArticles) {
+                for (var i = 0; i < resArticles.length; i++)
+                {
+                    var article = $("<div>");
+                    $(article).addClass("article");
+                    $(article).append("<br><a href='"+resArticles[i].link+"'><div class='article-title'>"+resArticles[i].title+"</div></a>");
+                    $(article).append("<br><div class='article-summary'>"+resArticles[i].summary+"</div><br>");
+                    $("#articles-place").append(article);
+                    $("#articles-place").append("<br>");
+                    var buttons = $("<div>");
+                    $(buttons).addClass("buttons");
+                    $(buttons).append("<button class='save-button' data-articleId='"+resArticles[i]._id+"'>Save</button>");
+                    $("#articles-place").append(buttons);
+
+                }
+            })
         });
-    })
+    });
+
+    //WHEN USER PRESSES A SAVE BUTTON
+    $("#articles-place").on("click", ".save-button", function() {
+        var articleId = $(this).attr("data-articleId");
+        $.ajax("/api/savedArticles/", {
+            type: "POST",
+            data: {
+                articleId: articleId
+            }
+        });
+    });
 })
---
+++ routes/api/api-saved.js
@@ -0,0 +1,9 @@
+// module.exports = function(router) {
+//     router.get("/scrape", function(req, res) {
+//         controller.scrapeArticles();
+//         res.end();
+//     })
+//     router.get("/api/articles", function(req, res) {
+//         headlineController.displayAllAPI(res);
+//     })
+// }
